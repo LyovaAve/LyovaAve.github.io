5f49fc0e9e453a03a604f287480514b9027afd71
LyovaAve <57953484+LyovaAve@users.noreply.github.com>
2019-12-23 19:01:49 +0300

    Add files via upload

---
+++ Armhunt/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.sale__slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        infinite: true,
+        arrows: false,
+        speed: 500,
+        slidesToShow: 1,
+        cssEase: 'linear'
+    });
+  });
