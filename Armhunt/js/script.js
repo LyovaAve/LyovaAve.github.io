85a617b82b440e81d54dda0cfefff77945f0c012
LyovaAve <57953484+LyovaAve@users.noreply.github.com>
2019-12-23 21:06:45 +0300

    Add files via upload

--- Armhunt/js/script.js
+++ Armhunt/js/script.js
@@ -9,4 +9,42 @@ $(document).ready(function(){
         slidesToShow: 1,
         cssEase: 'linear'
     });
+    $('.slider-plenty').slick({
+        dots: false,
+        infinite: false,
+        speed: 500,
+        arrows: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        prevArrow: "<img src='../img/new_product/left.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='../img/new_product/right.svg' class='next' alt='2'>",
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: false
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+      });
   });
